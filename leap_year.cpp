5621dafe5d9ab6f5fd04bc8f7b6cf51f0f32a337
Solaraeth <bpwandera@gmail.com>
2023-07-26 12:08:58 +0100

    Edit User input validity check added
    - Now checks if inputed year is after 1582
    - Still needs to check that years divisible by 100 are also divisible by
    400 before being labled as a leap year.

--- leap_year.cpp
+++ leap_year.cpp
@@ -4,19 +4,26 @@ int main() {
 
     int year = 0;
     
-    std::cout << "Input year: ";
+    //Asks user to input a year and assgins that to the variable year
+    std::cout << "Input year after 1582 (first year of Gregorian calender): ";
     std::cin >> year;
 
-    if (year % 4 == 0) {
-
+    //if year is after 1582 and the modulo of year is equal to 0 the user is told that their year is a leap year
+    if (year > 1582 && year % 4 == 0) {
+        
         std::cout << "The year " << year << " is a leap year\n";
     }
 
-    else {
+    //if the modulo of the year is not equal to 0 the user is told that their year is not a leap year
+    else if (year > 1582 && year % 4 != 0) {
 
         std::cout << "The year " << year << " is not a leap year\n";
     }
 
+    else {
+
+        std::cout << "Invalid input\nThe year must be greater than 1582\n";
+    }
     
 
     
