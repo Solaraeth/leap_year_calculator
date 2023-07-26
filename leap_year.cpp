0fcf617d49b8deaf48d1deb8da0458c7aa72902e
Solaraeth <bpwandera@gmail.com>
2023-07-26 13:35:20 +0100

    Edit Filters out further non leap years
    -Now correctly classifies years that arent' divisible by both 100 and
    400 as non leap years
    -Requires loop for when user inputs invalid year

--- leap_year.cpp
+++ leap_year.cpp
@@ -4,30 +4,36 @@ int main() {
 
     int year = 0;
     
-    //Asks user to input a year and assgins that to the variable year
+    // Asks user to input a year and assgins that to the variable year
     std::cout << "Input year after 1582 (first year of Gregorian calender): ";
     std::cin >> year;
 
-    //if year is after 1582 and the modulo of year is equal to 0 the user is told that their year is a leap year
-    if (year > 1582 && year % 4 == 0) {
+    // Outputs error message if user inputs year less than 1582
+    if (year < 1582) {
+
+        std::cout << "Error: invalid input\nThe year must be greater than 1582\n";
+
+    }
+
+    // Identifies leap years divisible by both 100 and 400 and outputs as true for user
+    else if (year % 100 == 0 && year % 400 == 0) {
         
         std::cout << "The year " << year << " is a leap year\n";
     }
 
-    //if the modulo of the year is not equal to 0 the user is told that their year is not a leap year
-    else if (year > 1582 && year % 4 != 0) {
+    // Identifies leap years divisible by 4 but not 100 and outputs as true for user
+    else if (year % 100 != 0 && year % 4 == 0) {
 
-        std::cout << "The year " << year << " is not a leap year\n";
+        std::cout << "The year " << year << " is a leap year\n";
+
     }
 
+    // Outputs false to user for the rest of the years
     else {
 
-        std::cout << "Invalid input\nThe year must be greater than 1582\n";
+        std::cout << "The year " << year << " is not a leap year\n";
+
     }
     
-
-    
-
-
 
 }
