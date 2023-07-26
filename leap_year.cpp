c3d04cce51a5b109d199c2efe5663a5985138b97
Solaraeth <bpwandera@gmail.com>
2023-07-26 13:42:43 +0100

    Edit added fullstops to output messages

--- leap_year.cpp
+++ leap_year.cpp
@@ -11,27 +11,27 @@ int main() {
     // Outputs error message if user inputs year less than 1582
     if (year < 1582) {
 
-        std::cout << "Error: invalid input\nThe year must be greater than 1582\n";
+        std::cout << "Error: invalid input\nThe year must be greater than 1582.\n";
 
     }
 
     // Identifies leap years divisible by both 100 and 400 and outputs as true for user
     else if (year % 100 == 0 && year % 400 == 0) {
         
-        std::cout << "The year " << year << " is a leap year\n";
+        std::cout << "The year " << year << " is a leap year.\n";
     }
 
     // Identifies leap years divisible by 4 but not 100 and outputs as true for user
     else if (year % 100 != 0 && year % 4 == 0) {
 
-        std::cout << "The year " << year << " is a leap year\n";
+        std::cout << "The year " << year << " is a leap year.\n";
 
     }
 
     // Outputs false to user for the rest of the years
     else {
 
-        std::cout << "The year " << year << " is not a leap year\n";
+        std::cout << "The year " << year << " is not a leap year.\n";
 
     }
     
