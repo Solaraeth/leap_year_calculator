5732eb56a7ede7d3b016eefb52e73441c2a67ff8
Solaraeth <bpwandera@gmail.com>
2023-07-26 16:10:01 +0100

    Edit now asks user to retry again if input invalid

--- leap_year.cpp
+++ leap_year.cpp
@@ -5,20 +5,26 @@ int main() {
     int year = 0;
     
     // Asks user to input a year and assgins that to the variable year
+
     std::cout << "Input year after 1582 (first year of Gregorian calender): ";
     std::cin >> year;
 
     // Outputs error message if user inputs year less than 1582
-    if (year < 1582) {
 
-        std::cout << "Error: invalid input\nThe year must be greater than 1582.\n";
+    while (year < 1582) {
+
+        std::cout << "Error: invalid input\nThe year must be greater than 1582.\nTry again: ";
+        std::cin >> year;
 
     }
 
+
     // Identifies leap years divisible by both 100 and 400 and outputs as true for user
-    else if (year % 100 == 0 && year % 400 == 0) {
+
+    if (year % 100 == 0 && year % 400 == 0) {
         
         std::cout << "The year " << year << " is a leap year.\n";
+
     }
 
     // Identifies leap years divisible by 4 but not 100 and outputs as true for user
